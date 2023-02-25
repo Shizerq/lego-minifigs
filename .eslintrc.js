d170dadbfea531dd78532cff8e4be41fce230cb2
Maciej Synowski <34197298+Shizerq@users.noreply.github.com>
2023-02-25 14:54:28 +0100

    feat: configure eslint, format files

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "prettier/prettier": [2],
+    "react/jsx-filename-extension": [2, { extensions: [".tsx"] }],
+    // reports wrong errors for React Native stylesheets
+    "no-use-before-define": "off",
+  },
+};
